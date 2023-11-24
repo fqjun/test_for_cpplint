e91442fdfae10f366c7ffc660cf23f1cc80fb38e
fqjun <1767851382@qq.com>
2023-11-24 18:21:08 +0800

    test

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 
 int main(int argc, char const* argv[]) {
   std::cout << "hello world!" << std::endl;
+  if (1) {
+    std::cout << "hell" << std::endl;
+    std::cout << "hell" << std::endl;
+    std::cout << "hell" << std::endl;
+    std::cout << "hell" << std::endl;
+    std::cout << "hell" << std::endl;
+  } else {
+    std::cout << "hell" << std::endl;
+  }
   return 0;
 }
